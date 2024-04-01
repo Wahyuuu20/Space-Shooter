f1a7cffa002a84aaee1d83a6b608115c08e9f3cd
Wahyu <wahyuu290400@gmail.com>
2024-03-31 17:24:05 -0700

    Menambahkan fungsi Damage, Death dan Menghapus Node

--- Script/PlayerScirpt.gd
+++ Script/PlayerScirpt.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 
 
 var MovementSpeed : float = 100
+var life: int  = 10
 
 func _process(delta):
+	
+	#Animate Damage Player
+	
+	
 	
 	#Movement
 	if Input.is_action_pressed("left"):
@@ -18,8 +23,22 @@ func _process(delta):
 	Gun()	
 		
 
+
+func Damge(values):
+	life -= values
+	if life < 0:
+		Death()
+		
+
+
 func Gun():
 	if Input.is_action_just_pressed("Primary"):
 		print("Primary")
 	if Input.is_action_just_pressed("Secondary"):
 		print("Secondary")			
+		
+func Death():
+	queue_free()		
+			
+func DeleteNode():
+	queue_free()		
