8c673f8ce51d3aec05606ac9beb1ea162648a29b
Wahyu <wahyuu290400@gmail.com>
2024-04-05 00:40:14 -0700

    Menambahkan fungsi speed gun Projectile-player.gd

--- Script/projectile-player.gd
+++ Script/projectile-player.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
 
-var speedProjectile : float = 20
+var speedProjectile : float = 5
+var direction := Vector2.ZERO
+
+func _physics_process(delta):
+	if direction != Vector2.ZERO:
+		var velocity = direction * speedProjectile 
+		global_position += velocity
+
+
+func set_direction(direction: Vector2):
+	self.direction = direction
 
 func Projectile_screen_exited():
 	queue_free()
