57b62593bb1b8c7dd457def875d007111e257af0
unknown <wahyuu290400@gmail.com>
2024-05-14 05:03:08 -0700

    Menambahkan folder script mainmenuUi dan script

---
+++ Script/MainMenuUi/MainMenu.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+func start_button():
+	pass
+
+func help_button():	
+	pass # Replace with function body.
+	
+func quit_button():
+	pass # Replace with function body.
+	
+	
+
+
+
+
