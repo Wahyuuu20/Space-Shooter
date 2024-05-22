251be507e70c10de2ed3479c7e106defaf691fcf
unknown <wahyuu290400@gmail.com>
2024-05-22 00:31:17 -0700

    Menambahkan folder Weapon_resource

---
+++ Scene/Gun/Weapon_Resource/WeaponResource.gd
@@ -0,0 +1,18 @@
+extends Resource
+
+class_name Weapon_Resource
+
+@export var weapon_name : String
+@export var active_anim : String
+@export var shoot_anim : String
+@export var reload_anim : String
+@export var deactive_anim : String
+@export var Out_of_ammo_anim : String
+
+
+@export var current_ammo : int
+@export var reserve_ammo : int
+@export var magazine : int
+@export var Max_ammo : int
+
+@export var Auto_fire : bool
