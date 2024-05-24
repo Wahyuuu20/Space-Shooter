5908337ecb3ed3b52eb4cc898485c15d299ca418
Wahyu <wahyuu290400@gmail.com>
2024-05-24 04:47:36 -0700

    membuat resource mobile suit dan setiap mobile suit resource

---
+++ Scene/Gun/MobileSuit/MobileSuit_Resource/MobileSuitResource.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name  Mobile_Suit_Resource
+
+@export var mobile_suit_name : String
+@export var suit_active_ani : String
+@export var suit_deactive_ani : String
+@export var suit_destroy_ani : String
+
+
+
