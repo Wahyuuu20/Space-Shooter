864330cc43dcd5edae1178505993f78df0b69fe2
Wahyu <wahyuu290400@gmail.com>
2024-06-02 16:31:55 -0700

    penambahan scene projectile baru seperti machine gun shotgun dan misile launcher dan serta script masing masing senjata

---
+++ Scene/Projectile/MachineProjectile.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var Damage: int = 0 
+var projectile_speed : float = 0
+var direction := Vector2(0,-1)
+
+func _physics_process(delta):
+	global_position += direction.rotated(rotation) * projectile_speed * delta
+	
+func Projectile_screen_exited():
+	queue_free()
+
+
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Damageable"):
+		area.Damage(Damage)
