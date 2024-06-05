a84b46783b2797a09482759753c6c3336bc30822
Wahyu <wahyuu290400@gmail.com>
2024-06-04 19:47:36 -0700

    menambahkan load scene bullet efect ketika terkena enemy

--- Scene/Projectile/MachineProjectile.gd
+++ Scene/Projectile/MachineProjectile.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var bullet_effect = preload("res://Scene/Gun/BulletEffect/Bullet_Efect_Machine.tscn")
 var Damage: int = 0 
 var projectile_speed : float = 0
 var direction := Vector2(0,-1)
@@ -16,3 +17,7 @@ func Projectile_screen_exited():
 func _on_area_entered(area):
 	if area.is_in_group("Damageable"):
 		area.Damage(Damage)
+		var effect = bullet_effect.instantiate()
+		effect.global_position = global_position
+		get_tree().current_scene.add_child(effect)
+		queue_free()
--- Script/projectile-player.gd
+++ Script/projectile-player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var bullet_efect = preload("res://Scene/Gun/BulletEffect/Bullet_Effect_Assault.tscn")
 var Damage: int = 0 
 var projectile_speed: float = 0
 var direction := Vector2(0,-1)
@@ -11,7 +12,11 @@ func _physics_process(delta):
 func _on_area_entered(area):
 	if area.is_in_group("Damageable"):
 		area.Damage(Damage)
-
+		var effect = bullet_efect.instantiate()
+		effect.global_position = global_position
+		get_tree().current_scene.add_child(effect)
+		
+		queue_free()
 
 	
 func Projectile_screen_exited():
